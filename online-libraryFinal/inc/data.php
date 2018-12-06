42937c953a22627d55364e8e34b7d103c4ae814a
davidmccalla <45578557+davidmccalla@users.noreply.github.com>
2018-12-05 19:37:57 -0500

    Add files via upload

---
+++ online-libraryFinal/inc/data.php
@@ -0,0 +1,164 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Cat in the Hat",
+	"img" => "img/media/catinthehat.jpg",
+    "genre" => "Childrens",
+    "format" => "Paperback",
+    "year" => 1973,
+    "category" => "Books",
+    "authors" => [
+        "Dr. Seuss"
+    ],
+    "publisher" => "Penguin Random House LLC., Houghton, Mifflin Company",
+    "isbn" => '978-0-7172-6059-1'
+];
+$catalog[102] = [
+    "title" => "Hop On Pop",
+    "img" => "img/media/hoponpop.jpg",
+    "genre" => "Childrens",
+    "format" => "Paperback",
+    "year" => 1963,
+    "category" => "Books",
+    "authors" => [
+        "Dr. Seuss"
+    ],
+    "publisher" => "Penguin Random House LLC.",
+    "isbn" => '9781932256697'
+];
+$catalog[103] = [
+    "title" => "One Fish Two Fish Red Fish Blue Fish",
+    "img" => "img/media/onefishtwofish.jpg",
+    "genre" => "Childrens",
+    "format" => "Paperback",
+    "year" => 1960,
+    "category" => "Books",
+    "authors" => [
+        "Dr. Seuss"
+    ],
+    "publisher" => "Penguin Random House LLC.",
+    "isbn" => '9781932256697'
+];
+$catalog[104] = [
+    "title" => "The Lorax",
+    "img" => "img/media/thelorax.jpg",
+    "genre" => "Childrens",
+    "format" => "Paperback",
+    "year" => 1971,
+    "category" => "Books",
+    "authors" => [
+        "Dr. Seuss"
+    ],
+    "publisher" => "Penguin Random House LLC.",
+    "isbn" => '9780739339275'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Beverly Hills Cop",
+    "img" => "img/media/beverlyhillscop.jpg",
+    "genre" => "Drama/Thriller",
+    "format" => "DVD, Streaming",
+    "year" => 1984,
+    "category" => "Movies",
+    "director" => "Martin Brest",
+    "writers" => [
+        "Daniel Petrie Jr.",
+        "Danilo Bach"
+    ],
+    "stars" => [
+        "Eddie Murphy",
+        "Judge Reinhold",
+        "John Ashton"
+    ]
+];
+$catalog[202] = [
+    "title" => "Pulp Fiction",
+    "img" => "img/media/pulpfiction.jpg",
+    "genre" => "Drama/Crime",
+    "format" => "DVD, Streaming",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Quentin Tarantino",
+    "writers" => [
+        "Quentin Tarantino"
+    ],
+    "stars" => [
+        "Sam L. Jackson",
+        "John Travolta",
+        "Uma Thurman",
+    ]
+];
+$catalog[203] = [
+    "title" => "The Dark Knight",
+    "img" => "img/media/thedarkknight.jpg",
+    "genre" => "Drama/Thriller",
+    "format" => "DVD, Streaming",
+    "year" => 2008,
+    "category" => "Movies",
+    "director" => "Christopher Nolan",
+    "writers" => [
+        "Christopher Nolan",
+        "Jonathan Nolan",
+    ],
+    "stars" => [
+        "Christian Bale",
+        "Heath Ledger",
+        "Gary Oldman",
+    ]
+];
+$catalog[204] = [
+    "title" => "The Incredibles",
+    "img" => "img/media/theincredibles.jpg",
+    "genre" => "Animated/Family",
+    "format" => "DVD, Streaming",
+    "year" => 2004,
+    "category" => "Movies",
+    "director" => "Brad Bird",
+    "writers" => [
+        "Brad Bird"
+    ],
+    "stars" => [
+        "Craig T. Nelson",
+        "Holly Hunter",
+        "Samuel L. Jackson",
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Blue Chips",
+    "img" => "img/media/bluechips.jpg",
+    "genre" => "Rap",
+    "format" => "Streaming",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Action Bronson"
+];
+$catalog[302] = [
+    "title" => "Aquemini",
+    "img" => "img/media/aquemini.jpg",
+    "genre" => "Rap",
+    "format" => "Vinyl, Streaming",
+    "year" => 1998,
+    "category" => "Music",
+    "artist" => "Outkast"
+];
+$catalog[303] = [
+    "title" => "Operation Doomsday",
+    "img" => "img/media/mfdoom.jpg",
+    "genre" => "Rap",
+    "format" => "Streaming, Vinyl",
+    "year" => 1999,
+    "category" => "Music",
+    "artist" => "MF DOOM"
+];
+$catalog[304] = [
+    "title" => "Pinata",
+    "img" => "img/media/pinata.jpg",
+    "genre" => "Rap",
+    "format" => "Vinyl, Streaming",
+    "year" => 2014,
+    "category" => "Music",
+    "artist" => "Freddie Gibbs",
+								"Madlib"
+];
